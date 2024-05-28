ac586d977e739eb6ec03a41109807444643d5ffd
Francisca Leal <falealgonz@gmail.com>
2024-05-28 11:15:06 -0400

    act2

--- BuclesFor/bucles_for_basico1.py
+++ BuclesFor/bucles_for_basico1.py
@@ -12,10 +12,12 @@ for i in range(2,501):
 # 3 - imprime los números enteros del 1 al 100. Si es divisible por 5 imprime “ice ice” en vez del número. Si es divisible por 10, imprime “baby”
 print("#############################3######################################")
 for i in range(1,101):
-    if i%5 == 0:
-        print("ice ice")
-    elif i%10 == 0:
+    if i%10 == 0:
         print("baby")
+    elif i%5 == 0:
+        print("ice ice")
+    else:
+        print(i)
 
 # 4 - suma los números pares del 0 al 500,000 e imprime la suma total. (Sorpresa, será un número gigante).
 print("#############################4######################################")
@@ -29,7 +31,7 @@ print(par)
 print("#############################5######################################")
 for i in range(2024,0,-3):
     print(i)
-    
+
 # 6 -  establece tres variables: numInicial, numFinal y multiplo. Comenzando en numInicial y pasando por numFinal, imprime los números enteros que sean múltiplos de multiplo. Por ejemplo: si numInicial = 3, numFinal = 10, y multiplo = 2, el bucle debería de imprimir 4, 6, 8, 10 (en líneas sucesivas).
 print("#############################6######################################")
 numInicial = 2
