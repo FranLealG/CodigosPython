e68577c088eb1c0bd097d9f55f48654ac27d5483
Francisca Leal <falealgonz@gmail.com>
2024-06-04 17:48:29 -0400

    Act Tarea TarjetaCredito

--- TarjetaCredito/TarjetaCredito.py
+++ TarjetaCredito/TarjetaCredito.py
@@ -1,9 +1,13 @@
 class TarjetaCredito:
 
+    lista_tarjetas = []
+
     def __init__(self, saldo_pagar, limite_credito, intereses):
         self.saldo_pagar = saldo_pagar
         self.limite_credito = limite_credito
         self.intereses = intereses
+
+        TarjetaCredito.lista_tarjetas.append(self)
 
     
     def compra(self, monto):
@@ -25,3 +29,16 @@ class TarjetaCredito:
     def cobrar_interes(self):
         self.saldo_pagar += self.saldo_pagar * self.intereses
         return self
+
+    #BONUS
+    def imprimir(self):
+        print(f"El saldo a pagar es de: ${self.saldo_pagar}\nEl límite es de: ${self.limite_credito}\nLos intereses son de: {self.intereses}")
+
+    @classmethod
+    def info_instancias(cls):
+        cont = 1
+        for dato in cls.lista_tarjetas:
+            print(f"####### Tarjeta {cont} #######")
+            cont+=1
+            dato.imprimir()
+    
